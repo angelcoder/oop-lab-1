1e82fddbd080c03ff5ba6cd84284c9ce10d029ab
angelcoder <31827866+angelcoder@users.noreply.github.com>
2018-09-19 20:23:54 +0300

    Update functions.cpp

--- oop-lab-1/oop-lab-1/functions.cpp
+++ oop-lab-1/oop-lab-1/functions.cpp
@@ -3,7 +3,7 @@
 //  oop-lab-1
 //
 //  Created by Anhelina Lohvina on 19.09.2018.
-//  Copyright © 2018 Anhelina Lohvina. All rights reserved.
+//  Copyright © 2018 Anhelina Lohvina. All rights reserved
 //
 
 #include "functions.hpp"
