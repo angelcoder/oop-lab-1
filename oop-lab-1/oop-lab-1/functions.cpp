ce220674efba4ca9ac4050f3e8edc57387e7d94e
angelcoder <lohvina.anhelina98.shostka@gmail.com>
2018-09-19 20:18:24 +0300

    created functions

---
+++ oop-lab-1/oop-lab-1/functions.cpp
@@ -0,0 +1,9 @@
+//
+//  functions.cpp
+//  oop-lab-1
+//
+//  Created by Anhelina Lohvina on 19.09.2018.
+//  Copyright © 2018 Anhelina Lohvina. All rights reserved.
+//
+
+#include "functions.hpp"
---
+++ oop-lab-1/oop-lab-1/functions.hpp
@@ -0,0 +1,14 @@
+//
+//  functions.hpp
+//  oop-lab-1
+//
+//  Created by Anhelina Lohvina on 19.09.2018.
+//  Copyright © 2018 Anhelina Lohvina. All rights reserved.
+//
+
+#ifndef functions_hpp
+#define functions_hpp
+
+#include <stdio.h>
+
+#endif /* functions_hpp */
