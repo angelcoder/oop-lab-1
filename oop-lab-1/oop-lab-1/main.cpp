e1257ae04d6d34f25d8bb0a3f63ff3f6cbfc93c3
angelcoder <lohvina.anhelina98.shostka@gmail.com>
2018-09-19 20:14:38 +0300

    created project

---
+++ oop-lab-1/oop-lab-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  oop-lab-1
+//
+//  Created by Anhelina Lohvina on 19.09.2018.
+//  Copyright © 2018 Anhelina Lohvina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
